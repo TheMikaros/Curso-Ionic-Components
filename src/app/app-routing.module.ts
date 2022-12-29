375a13656a49d5a2bf4630e1bf8c560a07d51091
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-29 10:20:06 -0600

    Solve date
    solve date. and re create funtions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
   {
     path: 'datetime',
     loadChildren: () => import('./pages/datetime/datetime.module').then( m => m.DatetimePageModule)
+  },
+  {
+    path: 'grid',
+    loadChildren: () => import('./pages/grid/grid.module').then( m => m.GridPageModule)
   }
 ];
 
--- src/app/pages/datetime/datetime.page.ts
+++ src/app/pages/datetime/datetime.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-datetime',
@@ -10,33 +11,44 @@ export class DatetimePage implements OnInit {
   fechaNaci: Date = new Date();
 
   customYearValues = [2025, 2020, 2016, 2008, 2004, 2000, 1996];
-  customPickerOptions = {
-    buttons: [
-      {
-        text: 'Hola',
-        handler: ( event: any ) => {
-          console.log(event);
-        }
-      },
-      {
-        text: 'Mundo',
-        handler: () => {
-          console.log('log!')
-        }
-      },
-    ]
-  }
 
-  constructor() { }
+  constructor(private pickerController: PickerController) { }
 
   ngOnInit() {
   }
-
-  cambioFecha( event: { detail: { value: string | number | Date; }; } ) {
-
-    console.log(event);
-    console.log( new Date( event.detail.value ) );
-
+  selectedAnimal: string = "";
+  async presentPicker() {
+    const picker = await this.pickerController.create({
+      buttons: [
+        {
+          text: 'Confirm',
+          handler: (selected) => {
+            console.log(selected);
+          }
+        }
+      ],
+      columns: [
+        {
+          name: 'animal',
+          options: [
+            { text: 'Dog', value: 'dog' },
+            { text: 'Cat', value: 'cat' },
+            { text: 'Bird', value: 'bird' },
+          ],
+        },
+        {
+          name: 'size',
+          options: [
+            { text: 'small', value: 'small' },
+            { text: 'middle', value: 'middle' },
+            { text: 'big', value: 'big' },
+          ]
+        }
+      ]
+    });
+    await picker.present();
   }
+
+
 
 }
