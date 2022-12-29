1bc14c56527f6652d3a1997274584d7271d0ce9b
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-29 13:55:37 -0600

    Loading
    add function load page

--- src/app/pages/loading/loading.page.ts
+++ src/app/pages/loading/loading.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-loading',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoadingPage implements OnInit {
 
-  constructor() { }
+
+  loading!: HTMLIonLoadingElement;
+
+
+  constructor( private loadingCtrl: LoadingController ) { }
 
   ngOnInit() {
+  }
+
+  mostrarLoading() {
+    this.presentLoading('I love Yamato');
+
+    setTimeout(() => {
+      this.loading.dismiss();
+    }, 2000);
+  }
+
+  async presentLoading( message: string) {
+
+    this.loading = await this.loadingCtrl.create({
+      message,
+      cssClass: 'custom-loading',
+
+    });
+
+    await this.loading.present();
+
   }
 
 }
