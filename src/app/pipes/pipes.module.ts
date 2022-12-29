7b9ba649f8a6ab5e44858882d77e142953688095
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-29 16:08:26 -0600

    Ion searchbar
    Create add page search page
    create function searchj page

---
+++ src/app/pipes/pipes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { FiltroPipe } from './filtro.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    FiltroPipe
+  ],
+  imports: [
+
+  ],
+  exports: [
+    FiltroPipe
+  ]
+})
+export class PipesModule { }
